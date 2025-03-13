28472d1f571bbf2cb5219ed65d9a1b083d73ceb2
zzangkkmin <zzangkkmin@naver.com>
2025-03-13 14:49:20 +0900

    임시 프로젝트 셋팅 커밋

---
+++ com.bulbas23r.client.deliverymanager/src/main/java/com/bulbas23r/client/deliverymanager/DeliverymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.bulbas23r.client.deliverymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverymanagerApplication.class, args);
+	}
+
+}
---
+++ com.bulbas23r.client.deliverymanager/src/test/java/com/bulbas23r/client/deliverymanager/DeliverymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulbas23r.client.deliverymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
